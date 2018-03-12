925f6493ab4d06d8eb41aa27eb0fbd77fbaaec83
yevhen orlov <yevhenorlov@gmail.com>
2018-03-12 13:12:13 +0200

    add pizza-app subfolder

---
+++ pizza-app/dist/bundle.js
@@ -0,0 +1,128 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _app = __webpack_require__(1);
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+__webpack_require__(2);
+
+// move to a separate helpers library later
+Number.prototype.pad = function (size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {
+    s = '0' + s;
+  }
+  return s;
+};
+
+{
+  var updateClock = function updateClock(clock) {
+    var d = new Date(),
+        h = d.getHours().pad(),
+        m = d.getMinutes().pad(),
+        s = d.getSeconds().pad(),
+        time = h + ':' + m + ':' + s;
+    clock.innerHTML = time;
+  };
+
+  var clock = document.getElementById('clock');
+
+  window.setInterval(function () {
+    updateClock(clock);
+  }, 500);
+}
+
+exports.default = App;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
---
+++ pizza-app/webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css',
+});
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        // babel-loader with 'env' preset
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader', options: { presets: ['env'] } },
+      },
+      {
+        // sass module with source maps
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader', // translates CSS into CommonJS
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'sass-loader', // compiles Sass to CSS
+              options: { sourceMap: true },
+            },
+          ],
+          fallback: 'style-loader', // creates style nodes from JS strings
+        }),
+      },
+      {
+        // file-loader(for images)
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/',
+            },
+          },
+        ],
+      },
+      {
+        // file-loader(for fonts)
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      /*{
+        // svg-loader
+        test: /\.svg$/,
+        use: ['svg-loader'],
+      },*/
+    ],
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
+
+module.exports = config;
